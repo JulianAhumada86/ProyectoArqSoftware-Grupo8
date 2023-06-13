e56aade0f66ddc9f59dc53e930119e0ac9d1436e
Agustin Glaiel <agustinglaiel@gmail.com>
2023-06-12 23:14:43 -0300

    user controller

--- go-api/controllers/user/controller_user.go
+++ go-api/controllers/user/controller_user.go
@@ -1,11 +1,10 @@
 package user
 
 import (
-	"net/http"
-	"strconv"
-
 	"go-api/dto/users_dto"
 	se "go-api/services"
+	"net/http"
+	"strconv"
 
 	log "github.com/sirupsen/logrus"
 
@@ -14,39 +13,53 @@ import (
 
 // Controlador para obtener un usuario por su ID
 func GetUserById(ctx *gin.Context) {
-	/*controllers.VerificacionToken()(ctx)
-	if err := ctx.Errors.Last(); err != nil {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
-	}
-
-	userID := ctx.GetInt("user_id")
-	if !controllers.Admin(userID){
-		ctx.JSON(http.StatusForbidden, gin.H{"error": "Debe tener permiso de administrador para realizar esta accion"})
-		return
-	}
-
-	log.Debug("User if to load: " + ctx.Param("id"))
-
+	log.Debug("User id to load: " + ctx.Param("id"))
 	id, _ := strconv.Atoi(ctx.Param("id"))
-	var userDto userdto.UserDto
 
+	var userDto users_dto.UserDto
 	userDto, err := se.UserService.GetUserById(id)
 
 	if err != nil {
-		ctx.JSON(err.Status(), err) //aca err status hay que implementarlo en manejo de errores
+		ctx.JSON(err.Status(), err)
 		return
-	}
-
-	ctx.JSON(http.StatusOK, userDto)*/
-
-	id, _ := strconv.Atoi(ctx.Param("id"))
-	userDto, err := se.UserService.GetUserById(id)
-
-	if err != nil {
-		log.Print("error")
 	}
 	ctx.JSON(http.StatusOK, userDto)
 
+	/*
+		controllers.VerificacionToken()(ctx)
+		if err := ctx.Errors.Last(); err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		}
+
+		userID := ctx.GetInt("user_id")
+		if !controllers.Admin(userID){
+			ctx.JSON(http.StatusForbidden, gin.H{"error": "Debe tener permiso de administrador para realizar esta accion"})
+			return
+		}
+
+		log.Debug("User if to load: " + ctx.Param("id"))
+
+		id, _ := strconv.Atoi(ctx.Param("id"))
+		var userDto userdto.UserDto
+
+		userDto, err := se.UserService.GetUserById(id)
+
+		if err != nil {
+			ctx.JSON(err.Status(), err) //aca err status hay que implementarlo en manejo de errores
+			return
+		}
+
+		ctx.JSON(http.StatusOK, userDto)
+	*/
+	/*
+		id, _ := strconv.Atoi(ctx.Param("id"))
+		userDto, err := se.UserService.GetUserById(id)
+
+		if err != nil {
+			log.Print("error")
+		}
+		ctx.JSON(http.StatusOK, userDto)
+	*/
 }
 
 func GetUsers(ctx *gin.Context) {
