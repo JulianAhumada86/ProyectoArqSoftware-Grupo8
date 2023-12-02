c73e61dc1d59d6329a82236a663dc015dc717cf3
PedroDiazR <pedrodiazromagnoli@gmail.com>
2023-12-02 12:16:55 -0300

    Confirmacion sin boton

--- frontend/src/src/Confirmation.js
+++ frontend/src/src/Confirmation.js
@@ -5,7 +5,7 @@ import { FaWifi, FaSwimmingPool, FaParking, FaDumbbell, FaSpa } from 'react-icon
 function Confirmation() {
   const hotel = {
     nombre: 'Hotel Ejemplo',
-    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
     fotos: [
       'https://www.maldronhotelnewcastle.com/wp-content/uploads/sites/25/2017/10/Room-Double-Single-Maldron-Newcastle-1-1680x860.jpg',
       'https://mcaleer-rushe.co.uk/site/wp-content/uploads/2019/05/Maldron-Hotel-Belfast-IntAirport-I.jpg',
@@ -24,8 +24,21 @@ function Confirmation() {
     <Container className="mt-5">
       <Row>
         <Col md={6}>
-          <h1>{hotel.nombre}</h1>
-          <p>{hotel.descripcion}</p>
+              <h1>{hotel.nombre}</h1>
+              <p>{hotel.descripcion}</p>
+            
+              <div className="mt-3">
+                <h5>Amenidades</h5>
+                <div className="d-flex flex-wrap">
+                  {hotel.amenities.map((amenity, index) => (
+                    <Card key={index} className="m-2" style={{ width: '4rem', height: '4rem' }}>
+                      <Card.Body style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                        <div style={{ fontSize: '1.75rem' }}>{amenity.icono}</div>
+                      </Card.Body>
+                    </Card>
+                  ))}
+                </div>
+              </div>
         </Col>
         <Col md={6}>
           <Carousel style={{ maxWidth: '100%', height: '100%' }}>
@@ -37,22 +50,10 @@ function Confirmation() {
           </Carousel>
         </Col>
       </Row>
-
-      <div className="mt-3">
-            <h5>Amenidades</h5>
-            <div className="d-flex flex-wrap">
-              {hotel.amenities.map((amenity, index) => (
-                <Card key={index} className="m-2" style={{ width: '12rem' }}>
-                  <Card.Body>
-                    {amenity.icono}
-                    <Card.Text>{amenity.nombre}</Card.Text>
-                  </Card.Body>
-                </Card>
-              ))}
-            </div>
-          </div>
+      
     </Container>
   );
+  
 }
 
 export default Confirmation;
